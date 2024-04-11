1f53797ba36786da6b79fcde5383b671b44781b1
Moritz Junge <moritz.junge@wps.de>
2024-04-11 11:52:44 +0200

    updated dashboard with new model data

--- src/main/java/de/moritzjunge/financer/controllers/DashboardController.java
+++ src/main/java/de/moritzjunge/financer/controllers/DashboardController.java
@@ -1,51 +1,82 @@
 package de.moritzjunge.financer.controllers;
 
+import de.moritzjunge.financer.model.Category;
+import de.moritzjunge.financer.model.FUser;
+import de.moritzjunge.financer.model.Household;
 import de.moritzjunge.financer.model.Transaction;
+import de.moritzjunge.financer.model.dtos.TransactionDTO;
+import de.moritzjunge.financer.services.HouseholdService;
 import de.moritzjunge.financer.services.TransactionService;
-import org.springframework.beans.factory.annotation.Autowired;
+import de.moritzjunge.financer.services.UserService;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 
 @Controller
 @RequestMapping("/dashboard")
 public class DashboardController {
 
-    private TransactionService transactionService;
+    private final TransactionService transactionService;
+    private final HouseholdService householdService;
+    private final UserService userService;
 
-    public DashboardController(TransactionService transactionService) {
+    public DashboardController(TransactionService transactionService, HouseholdService householdService, UserService userService) {
         this.transactionService = transactionService;
+        this.householdService = householdService;
+        this.userService = userService;
     }
 
     @GetMapping
-    public String getDashboard(Model model, @RequestParam(defaultValue = "") String transactionFilter) {
-        List<Transaction> filteredTransactions = transactionService.filterTransactions(transactionFilter);
-        model.addAttribute("filter", transactionFilter);
-        attachModelAttributes(model, filteredTransactions);
+    public String getDashboard(Model model) {
+        List<Transaction> filteredTransactions = transactionService.getTransactions();
+        attachModelAttributes(model, filteredTransactions, householdService.getHouseholds());
         return "dashboard";
     }
 
     @PostMapping
-    public String createTransaction(Model model, @ModelAttribute Transaction newTransaction) {
-//        transactions.add(new Transaction(Integer.parseInt(amount), description, LocalDate.now().minusDays(1), LocalDateTime.now()));
-        Transaction stampedTransaction = new Transaction(newTransaction.getAmount(), newTransaction.getDescription(), newTransaction.getTransactionDate(), LocalDateTime.now());
+    @Transactional
+    public String createTransaction(Model model, @ModelAttribute TransactionDTO newTransactionDTO) {
+        Household household = householdService.getHouseholdById(newTransactionDTO.getHouseholdId()).get();
+        FUser owner = userService.getUserById(newTransactionDTO.getOwnerId()).get();
+        FUser payer = userService.getUserById(newTransactionDTO.getPayerId()).get();
+        Category category = householdService.getCategoryById(newTransactionDTO.getCategoryId()).get();
+        Transaction stampedTransaction = new Transaction(
+                newTransactionDTO.getAmount(),
+                newTransactionDTO.getDescription(),
+                newTransactionDTO.getTransactionDate(),
+                LocalDateTime.now()
+        )
+                .setHousehold(household)
+                .setOwner(owner)
+                .setPayer(payer)
+                .setCategory(category);
         transactionService.addTransaction(stampedTransaction);
-//        attachModelAttributes(model, transactions);
         return "redirect:/dashboard";
     }
 
-    private void attachModelAttributes(Model model, List<Transaction> transactions) {
+    private void attachModelAttributes(Model model, List<Transaction> transactions, Collection<Household> households) {
+        Transaction newTransaction = new Transaction(100, "", LocalDate.now(), null);
+        FUser currentUser = userService.getAuthenticatedUser();
+        Household household = households.iterator().next();
+        Set<FUser> possiblePayers = household.getParticipants();
+
         model.addAttribute("zeitstempel", LocalDateTime.now().plusHours(10));
         model.addAttribute("transactions", transactions);
-        model.addAttribute("newTransaction", new Transaction(100, "", LocalDate.now(), null));
+        model.addAttribute("newTransaction",
+                TransactionDTO.fromEntities(newTransaction, currentUser, currentUser, household.getCategories().iterator().next(), household));
+        model.addAttribute("categories", household.getCategories());
+        model.addAttribute("households", households);
+        model.addAttribute("possiblePayers", possiblePayers);
     }
 
 }
