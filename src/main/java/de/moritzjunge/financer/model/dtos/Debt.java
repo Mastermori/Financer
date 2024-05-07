fb3abd092e7c5f9affd3eb424be0695697f8301d
Moritz Junge <moritz.junge@wps.de>
2024-05-07 09:58:33 +0200

    added debt model

---
+++ src/main/java/de/moritzjunge/financer/model/dtos/Debt.java
@@ -0,0 +1,11 @@
+package de.moritzjunge.financer.model.dtos;
+
+import de.moritzjunge.financer.model.FUser;
+
+public class Debt {
+
+    private FUser debtor;
+    private FUser creditor;
+    private int amount;
+
+}
